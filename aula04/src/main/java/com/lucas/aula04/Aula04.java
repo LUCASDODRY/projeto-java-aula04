b3f12fa2020af29823fe329d0b15843afe77e859
Lucas <lucas.lemos@duque.g12.br>
2025-08-25 19:40:31 -0300

    Primeiro commit da aplicação

---
+++ aula04/src/main/java/com/lucas/aula04/Aula04.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lucas.aula04;
+
+/**
+ *
+ * @author 05883
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int soma = a+b;
+    }
+}
